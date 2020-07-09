5540fa87b411f1fd61ced44adc1e2ce3618612b0
Joe Henson <JoeHenson@gmail.com>
2020-07-09 14:17:32 -0500

    Updates

---
+++ precache-manifest.f0e76fe95c61b8e6e1cfcbc185aa2468.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d31aa489a9d14bedf69958722fe0e2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5ab2d0c57d87e90cae61",
+    "url": "/my-app/static/css/main.ca4ae3bd.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/my-app/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab2d0c57d87e90cae61",
+    "url": "/my-app/static/js/main.cdce7781.chunk.js"
+  },
+  {
+    "revision": "c2a1dcc6438bccc67aae",
+    "url": "/my-app/static/js/runtime-main.813cd07b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
